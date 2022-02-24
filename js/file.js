d261754e8b1444e9809e7ca01628e0eeb482cd8d
unknown <essa@pencil-designs.com>
2022-02-24 11:47:21 +0200

    edits

--- js/file.js
+++ js/file.js
@@ -8,3 +8,13 @@ const password = document.querySelector('#id_password');
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
+const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+const passwordConfirm = document.querySelector('#id_confirm_password');
+ 
+  toggleConfirmPassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordConfirm.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordConfirm.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
